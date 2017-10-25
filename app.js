205c3db35b3b8825a731210a37edf620238f2acd
yhdeng <yhdeng@ctrip.com>
2017-10-25 13:45:10 +0800

    test

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
+/**
+ * 命令行入口处理方法, 
+ */
 
-var express = require('express');
-var cheerio = require('cheerio');
-var superagent = require('superagent');
+var params = process.argv;
 
-var app = express();
-
-// app.get('/', function(req, res, next){
-    superagent.get('http://sh.lianjia.com/ershoufang/sijing')
-            .end(function(err, res) {
-                if (err) {
-                    return next(err);
-                }
-
-                var $ = cheerio.load(res.text);
-
-                var items = [];
-                // console.log(res);
-                $('.js_fang_list li').each(function (idx, element) {
-                    var $element = $(element);
-                    console.log($element.html());
-                });
-
-            });
-// });
+params.splice(0, 2);
 
 
-// app.listen(3001, function() {
-//     console.log('app is listen port 3001')
-// });
+
+
+console.log(params);
