f5f51131be335309930f3d337bfb65bedee98a8b
yhdeng <yhdeng@ctrip.com>
2017-10-26 17:03:00 +0800

    test lianjia

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 /**
- * 命令行入口处理方法, 
+ * [params description]
+ * @type {[type]}
  */
+
+var fs = require('fs');
 
 var params = process.argv;
 
 params.splice(0, 2);
 
+if (params.length > 0) {
+    var cf = './src/controller/' + params[0] + '.js';
 
+    fs.exists(cf, function(exists) {
+        if (exists) {
+            var c = require(cf);
+            c.main();
 
+            // if (params.length > 1) {
+            //     c.main();
+            // } else {
+            //     c.main();
+            // }
 
-console.log(params);
+        } else {
+            console.log('Controller ' + params[0] + ' not exist.');
+        }
+    });
+}
